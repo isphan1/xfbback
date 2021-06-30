5d7a52f6cb5c77975a615ea9df43f2a939384a13
KILLER <isphan111@gmail.com>
2021-06-30 09:39:38 +0600

    b14

--- dammy.py
+++ dammy.py
@@ -8,41 +8,79 @@ from django.contrib.auth.models import User
 from faker import Faker
 import random
 
-def dammy(n):
+def user(n):
 
-    user = User.objects.select_related('profile').values('username')
-    post = Post.objects.select_related('profile').select_related('comment').values('text')
-    comment = Comment.objects.select_related('profile').select_related('post').values('text')
     fake = Faker()
-
-
     for _ in range(n):
-    #     user = fake.simple_profile()
+        user = fake.simple_profile()
+        profile = User.objects.create(
+            username = user['username'],
+            first_name = user['name'],
+            email = user['mail'],
+            is_active = True
+        )
 
-    #     profile = User.objects.create(
-    #         username = user['username'],
-    #         first_name = user['name'],
-    #         email = user['mail'],
-    #         is_active = True
-    #     )
+        profile.set_password("jnj")
+        profile.save()
 
-    #     profile.set_password("jnj")
-    #     profile.save()
 
+def post(n):
+    fake = Faker()
+    user = User.objects.select_related('profile').values('username')
+    for _ in n:
+        text = fake.sentence(5)
+        u = User.objects.get(username = user[random.randint(0,20)]['username'])
+        Post.objects.create(
+            user = u,
+            text = text,
+        )
+
+def comment(n):
+        fake = Faker()
+        user = User.objects.select_related('profile').values('username')
+        post = Post.objects.select_related('profile').select_related('comment').values('id')
+
+        u = User.objects.get(username = user[random.randint(0,20)]['username'])
+        p = Post.objects.get(id = post[random.randint(0,20)]['id'])
+        for _ in n:
+    
+            text = fake.sentence()
+            Comment.objects.create(
+                user=u,
+                post=p,
+                text=text
+            )
+def subComment(n):
+    fake = Faker()
+    user = User.objects.select_related('profile').values('username')
+    comment = Comment.objects.select_related('profile').select_related('post').values('id')
+    
+    for _ in n:
+        c = Comment.objects.get(text = comment[random.randint(0,500)]['id'])
+        u = User.objects.get(username = user[random.randint(0,20)]['username'])
         text = fake.sentence()
-        s = User.objects.get(username = user[random.randint(0,20)]['username'])
-        r = User.objects.get(username = user[random.randint(0,20)]['username'])
-        # c = Comment.objects.get(text = comment[random.randint(0,530)]['text'])
-        # SubComment.objects.create(
-        #     user = u,
-        #     comment = c,
-        #     text = text,
-        # )
+        SubComment.objects.create(
+            user = u,
+            comment = c,
+            text = text,
+        )
 
+def message(n):
+
+    fake = Faker()
+    user = User.objects.select_related('profile').values('username')
+    for _ in n:
+        u1 = User.objects.get(username = user[random.randint(0,20)]['username'])
+        u2 = User.objects.get(username = user[random.randint(0,20)]['username'])
+        text = fake.sentence()
         Message.objects.create(
-            sender = s,
-            receiver = r,
+            sender = u1,
+            receiver = u2,
             text = text
         )
 
-dammy(200)
+user(20)
+post(500)
+comment(2000)
+subComment(1000)
+message(1000)
